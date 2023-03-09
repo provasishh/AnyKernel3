79c1d9e0bf35a7d6b64e4855ccb5cceae42f4ce0
amackpro <prajwal.kumar008@gmail.com>
2023-03-09 21:37:10 +0530

    ready for RE54E4L1

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=OOF kernel by amack @ xda-developers
+kernel.string=MiNaZuki kernel by Amack
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=vayu
-device.name2=bhima
+device.name1=RMX3371
+device.name2=RE54E4L1
 device.name3=
 device.name4=
 device.name5=
-supported.versions=
+supported.versions=13
 supported.patchlevels=
 '; } # end properties
 
@@ -39,24 +39,5 @@ patch_vbmeta_flag=auto;
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
 write_boot;
-
-## vendor_boot shell variables
-block=/dev/block/bootdevice/by-name/vendor_boot;
-is_slot_device=auto;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-reset_ak;
-
-# vendor_boot install
-dump_boot;
-
-write_boot;
-## end vendor_boot install
 
